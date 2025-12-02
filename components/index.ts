7f1064d80ab7d6a39f14b8a07bceb6da82067bf7
shina5-source <shina5@hotmail.fr>
2025-12-02 05:15:04 +0100

    Fix components export

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
 export { default as Header } from './Header';
 export { default as BottomNav } from './BottomNav';
 export { default as Sidebar } from './Sidebar';
-export { default as TransactionForm } from './TransactionForm';
